6b512fcc79ce24dd653bd658a878e59c6624c53d
isko18 <toksonbaevislam2004@hmail.com>
2023-01-27 19:06:59 +0600

    first commit

---
+++ homework_2.py
@@ -0,0 +1,26 @@
+class Bank:
+    def __init__(self, __name) -> None:
+        self.__name = __name
+        self.__balance = 0
+    def money_x(self):
+        user = int(input("сколько вы хотите пополнить баланс: "))
+        self.__balance += user
+        return f"на вашем счёте: {self.__balance}, Имя влодельца: {self.__name}"
+    def _kill(self):
+        user = int(input("сколько вы хотите обноличить: "))
+        if self.__balance >= user:
+            self.__balance -= user
+            return f"вы обноличили счёт на {user} остаток на карте {self.__balance}"
+        else:
+            return f"На вашем счете недостаточно средств: {self.__balance}"
+    def __jacpot(self):
+        return f"ваш счет ужножок на 10, ваш счет: {self.__balance * 10}"
+    def user(self,name,__balanse):
+        self.name = name
+        self.__balanse = __balanse
+        return f"Имя: {self.name}, Balanse: {self.__balanse + self.__balance}"
+ban = Bank("askhat")
+print(ban.money_x())
+print(ban._kill())
+print(ban.user("bekbolot", 100))
+print(ban._Bank__jacpot())
---
+++ join.py
@@ -0,0 +1,4 @@
+class User:
+    login = "login" 
+    def __init__(self, name, nick_name, age,  ) -> None:
+        self.name = name 
