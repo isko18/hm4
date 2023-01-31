4d1779b11eafc43e1e7391c2b29963e241dd0a9a
isko18 <72527657+isko18@users.noreply.github.com>
2023-01-31 18:02:48 +0600

    first commit

--- hero_2.py
+++ hero_2.py
@@ -44,3 +44,26 @@ class Villain(SuperHero):
         return f"uron: {damage1 ** damage2}"
 villain = Villain("one", "ssd", "ok",100,"no")
 print(villain.crit(6,10)) 
+
+
+
+class A:
+    def __init__(self,name) -> None:
+        self.name = name
+class B:
+    def __init__(self,age) -> None:
+        self.age = age
+class C:
+    def a():
+        pass
+class D:
+    def b():
+        pass
+class Ask(A,B,C,D):
+    def __init__(self, name,age) -> None:
+        A.__init__(self,name)
+        B.__init__(self,age)
+    def __str__(self) -> str:
+        return f"{self.name}, {self.age}"
+ask = Ask("askhat",18)
+print(ask)
